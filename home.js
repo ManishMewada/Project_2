a828f748df1de58512d7674053ac4d781340145f
ManishMewada <manish.mewada@blobstation.com>
2023-02-04 22:05:37 +0530

    Add the button to picture so it will show next picture

--- home.js
+++ home.js
@@ -1,32 +1,49 @@
 const images = ["images/img1.jpg", "images/img2.jpg", "images/img3.jpg", "images/img4.jpg"];
-const captions = ["This is Image 1", "This is Image 2", "This is Image 3", "This Is Image 4"];
+const captions = ["Image of mountains with sunrise",
+    "Image of person posing at old place",
+    "Image of river with sunset",
+    "Image of river with forest"];
 
-var currentImage = document.querySelector("img");  
+var currentImage = document.querySelector("img");
+var currentCaption = document.querySelector("p");
 var currentIndex = 0;
-
-function next(){
+function pageReload(){
+    currentIndex = 0;
+    document.getElementById("image-name").innerHTML = captions[currentIndex];
+}
+function next() {
     currentIndex++;
-    if(currentIndex >= images.length){
+    if (currentIndex >= images.length) {
         currentIndex = 0;
         currentImage.src = images[currentIndex];
-    }else{
+        document.getElementById("image-name").innerHTML = captions[currentIndex];
+
+    } else {
         currentImage.src = images[currentIndex];
+        document.getElementById("image-name").innerHTML = captions[currentIndex];
+
+    }
+
+}
+
+function prev() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+        currentImage.src = images[currentIndex];
+        document.getElementById("image-name").innerHTML = captions[currentIndex];
+
+    } else {
+        currentImage.src = images[currentIndex];
+        document.getElementById("image-name").innerHTML = captions[currentIndex];
+
     }
 }
 
-function prev(){
-    currentIndex--;
-    if(currentIndex < 0){
-        currentIndex = images.length -1;
-        currentImage.src = images[currentIndex];
-    }else{
-        currentImage.src = images[currentIndex];
-    }
-}
-function shrink(){
+function shrink() {
     currentImage.style.width = (currentImage.clientWidth - 50) + "px";
 }
-function enlarge(){
+function enlarge() {
     currentImage.style.width = (currentImage.clientWidth + 50) + "px";
 }
 
